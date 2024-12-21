e73f0d25f21b3a5e21c6fe24d8a7913dd5406475
esevindik <enrsvndk@gmail.com>
2024-12-21 23:42:25 +0300

    #model#add-migration-initial@dbcontext#appsettings#

--- EntityFramework/EntityFramework/CodeFirst/Program.cs
+++ EntityFramework/EntityFramework/CodeFirst/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CodeFirst.DAL;
+using CodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+DbContextInitializer.Build();
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+    products.ForEach(p => { Console.WriteLine($"{p.Name}-{p.Price}-{p.Stock}"); });
+    
+    
+}
